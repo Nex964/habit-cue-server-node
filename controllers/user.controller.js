0e19a1a24942696ede225a8874a23e8a2c71f179
Glitch (glitch-hello-node) <none>
2024-12-20 23:46:22 +0000

    🚃🏃 Checkpoint ./controllers/user.controller.js:4484564/296

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,28 +2,17 @@ const UserModel = require("../models/user");
 
 const create = async (req, res) => {
   
-  const resData = [];
+  const 
   
-  for(let item of req.body.tasks){
+  const addedItem = await UserModel.findOneAndUpdate({ _id: item._id }, {...item}, { upsert: true, new: true, setDefaultsOnInsert: true });
     
-    if(item.createdBy === undefined){
-      item.createdBy = "yp"; 
-    }
-    item._id = item.id;
-    
-    delete item.id;
-    item.syncStatus = 1;
-    
-    const addedItem = await UserModel.findOneAndUpdate({ _id: item._id }, {...item}, { upsert: true, new: true, setDefaultsOnInsert: true });
-    
-    // const addedItem = await newTask.save();
-    if (addedItem.err) {
-      res.send("error saving new habit task");
-    } else {
-      resData.push(addedItem)
-    }
+  // const addedItem = await newTask.save();
+  if (addedItem.err) {
+    res.send("error saving new habit task");
+  } else {
+    resData.push(addedItem)
   }
-  
-  res.send(resData.map(uploadedItem => uploadedItem._id));
+
+res.send(resData.map(uploadedItem => uploadedItem._id));
   
 };
